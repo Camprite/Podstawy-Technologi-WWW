5e2142fab22a60d114ceebe8a1914abbb153ef30
Dawid Borkowski <dawid.borkowski37170@gmail.com>
2023-05-05 11:01:15 +0200

    Na wykladzie pierdololo

---
+++ zad5/script/dodajKontakt.js
@@ -0,0 +1,12 @@
+function dodajKontakt(){
+let imie = document.getElementById("INPimie").value;
+let nazwisko = document.getElementById("INPnazwisko").value;
+let mail = document.getElementById("INPmail").value;
+let wiek = document.getElementById("INPwiek").value;
+let dataur = document.getElementById("INPdataur")
+let plec = document.getElementById("plecM")
+// let plec = document.getElementById("plecK")
+console.log(plec)
+    
+
+}
---
+++ zad5/script/menu.js
@@ -0,0 +1,34 @@
+const x = document.getElementById("menu");
+function showMenu(){
+    const menuId = document.getElementById("mainMenu");
+    console.log(menuId);
+
+    if(menuId==null){
+    const nav = document.createElement("nav");
+    const a1 = document.createElement("a");
+    const a2 = document.createElement("a");
+    const a3 = document.createElement("a");
+    x.style.visibility="visible"
+    nav.setAttribute("id","mainMenu");
+    a1.textContent="Strona główna"       // NAZWA PIERWSZEJ OPCJI
+    a1.href="index.html"                //LINK DO PIERWSZEJ OPCJI    
+    a2.textContent="Kontakty"                                                   
+    a2.href="kontakty.html"
+    a3.textContent="TEST"
+    a3.href="galeria.html"
+    nav.append(a1,a2,a3);
+    x.append(nav);
+    const animation = nav.animate({ opacity: 1 }, 1000);
+   const promise = new Promise((res,rej)=>{
+    animation.play();
+    res();
+   })
+   setTimeout(()=>{
+    nav.style.opacity="1"
+   },999);
+}
+else{
+    menuId.parentNode.removeChild(menuId)
+    x.style.visibility="hidden"
+}
+}
